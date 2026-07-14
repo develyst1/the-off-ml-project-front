0ccf58dae31fdcb79889216f573606f3557c379a
Peaw <seaharat.p@gmail.com>
2026-07-14 15:15:50 +0700

    feat: implement standalone output configuration and asset copying script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   reactStrictMode: true,
+  allowedDevOrigins: [
+    "154.197.124.206",
+  ],
 };
 
 export default nextConfig;
