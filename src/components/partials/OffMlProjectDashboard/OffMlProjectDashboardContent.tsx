0d72b705b7d0a091db72348ce55ad4482833abc4
Peaw <seaharat.p@gmail.com>
2026-07-21 11:48:34 +0700

    ปรับขนาดปุ่มใน AutomationSettings จาก 15 เป็น 25

--- src/components/partials/OffMlProjectDashboard/OffMlProjectDashboardContent.tsx
+++ src/components/partials/OffMlProjectDashboard/OffMlProjectDashboardContent.tsx
@@ -1706,7 +1706,7 @@ function AutomationSettings({
                 })}
                 onChange={(page) => loadLogs({ page })}
                 radius="md"
-                size={15}
+                size={25}
                 siblings={1}
                 total={logsPage.totalPages}
                 value={logsPage.page}
